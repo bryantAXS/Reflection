6f12ada5177ccacac3c26eed5217a47f0ce1586a
Doug Black <dblack@americanbible.org>
2016-12-30 09:57:42 -0500

    Structured for ee2 and ee3

---
+++ ee3/system/user/add-ons/reflection/config.php
@@ -0,0 +1,12 @@
+<?php
+
+if (! defined('REFLECTION_NAME'))
+{
+	define('REFLECTION_NAME', 'Reflection');
+	define('REFLECTION_VERSION',  '1.0');
+}
+
+$config['name']    = REFLECTION_NAME;
+$config['version'] = REFLECTION_VERSION;
+
+#$config['nsm_addon_updater']['versions_xml'] = 'http://pixelandtonic.com/ee/releasenotes.rss/pt_switch';
